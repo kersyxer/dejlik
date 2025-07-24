0876f2855dc83ccb3c4dac359fa9d1fd57f5ce65
kersyxer <lukinchuk.vladyslav@gmail.com>
2025-07-24 14:07:57 +0200

    security update

--- src/main/java/com/project/config/SecurityConfig.java
+++ src/main/java/com/project/config/SecurityConfig.java
@@ -1,23 +1,82 @@
 package com.project.config;
 
+import com.project.security.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtUtil jwtUtil;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth.anyRequest().permitAll())
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/users/create").hasRole("ADMIN")
+                        .requestMatchers("/users/{id}").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter(), org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    private OncePerRequestFilter jwtAuthenticationFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(
+                    HttpServletRequest req,
+                    HttpServletResponse res,
+                    FilterChain chain
+            ) throws IOException, jakarta.servlet.ServletException {
+                String authHeader = req.getHeader(HttpHeaders.AUTHORIZATION);
+                if(authHeader != null && authHeader.startsWith("Bearer ")) {
+                    String token = authHeader.substring(7);
+                    try {
+                        if(!jwtUtil.isTokenExpired(token)) {
+                            Claims claims = jwtUtil.extractAllClaims(token);
+                            String userId = claims.getSubject();
+                            String role = claims.get("role", String.class);
+
+                            List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+                            Authentication auth = new UsernamePasswordAuthenticationToken(userId, null, authorities);
+                            org.springframework.security.core.context.SecurityContextHolder.getContext().setAuthentication(auth);
+                        }
+                    }catch (JwtException ignored) {
+
+                    }
+                }
+                chain.doFilter(req, res);
+            }
+        };
     }
 
     @Bean

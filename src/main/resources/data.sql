fd73d8d45132c27a7894e062bb191fe5f78fd3cd
kersyxer <lukinchuk.vladyslav@gmail.com>
2025-07-24 18:46:20 +0200

    security up

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO app_user (id, email, password, name, role)
-VALUES (
-           gen_random_uuid(),
-           'Admin@gmail.com',
-           '$2a$10$LmcJ3a8fsRhawYdXCtsXROvTN0.5prh0yYHsFEI9jK4OWwim1Tz4C',
-           'Admin',
-           'ADMIN'
-       );
